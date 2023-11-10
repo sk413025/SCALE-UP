53c0a28c84b871c09afcbc0a6d3aba54b5dda15d
tomwong12138 <reborn.1998.wong@gmail.com>
2023-11-10 23:38:24 +0800

    Refactor file paths and update model checkpoint

--- run.sh
+++ run.sh
@@ -5,6 +5,10 @@
 export PYTHONPATH="/home/lab/SCALE-UP:$PYTHONPATH"
 cd /home/lab/SCALE-UP
 python test_BadNets_cifar10.py
+
+newest_folder=$(ls -dt train_poisoned_CIFAR10_*/ | head -1)
+cp "${newest_folder}"ckpt_epoch_200.pth badnet/
+
 python torch_model_wrapper.py --model_type=benign
 python dataloader2tensor_CIFAR10.py
 python torch_model_wrapper.py --model_type=backdoor
