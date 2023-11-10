80360f7cae30c2ade945568f505de4bdd7333073
tomwong12138 <reborn.1998.wong@gmail.com>
2023-11-10 20:36:34 +0800

    Update BadNets test script to use CIFAR10 dataset.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 
 export PYTHONPATH="/home/lab/SCALE-UP:$PYTHONPATH"
 cd /home/lab/SCALE-UP
-python test_BadNets.py
+python test_BadNets_cifar10.py
 python torch_model_wrapper.py --model_type=benign
 python dataloader2tensor_CIFAR10.py
 python torch_model_wrapper.py --model_type=backdoor
